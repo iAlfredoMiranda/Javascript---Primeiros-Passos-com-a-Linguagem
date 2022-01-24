6b9a0cc864d2f794f32f6c1622b5b884e2344320
Alfredo <alfredo.s.miranda@hotmail.com>
2022-01-24 15:21:21 -0300

    first commit

---
+++ helloWorld.js
@@ -0,0 +1,10 @@
+console.log("Hello World, JavaScript!")
+console.log("Work with variables")
+
+const yearsAgo = 25
+
+console.log(yearsAgo)
+
+const yearsAgoSum = yearsAgo+2
+console.log(yearsAgoSum)
+console.log("I'm "+yearsAgo + "  years ago and in more than two year, I'll be "+ yearsAgoSum + "years ago")
---
+++ workWithVariables.js
@@ -0,0 +1,38 @@
+console.log("Work with Variables")
+
+const idade = 25
+let lastName = "Miranda"
+const fristName = "Alfredo"
+console.log(`Your name is ${lastName} ${fristName}`)
+
+const fullName = fristName + " " + lastName
+
+console.log(fullName)
+
+//Operation Aritiméticas
+
+console.log("Operações Aritiméticas")
+
+console.log(2 + 2)
+console.log(10 + 8 * 2)
+console.log((10 + 8) * 2)
+
+var sum = 2 + 2
+var sub = 2 - 2
+var div = 2 / 2
+var mult = 2 * 2
+
+console.log(sum)
+console.log(sub)
+console.log(div)
+console.log(mult)
+
+//Type Convertion
+
+console.log("Type Convertion")
+
+console.log("ano " + 2020);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+console.log(parseFloat("7") / parseFloat("2"));
+console.log(6.5);
